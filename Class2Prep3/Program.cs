39160882b4c4d077ec1fe79c7897e1f8d8db8fbd
Daniel <danielbrogers594@gmail.com>
2018-02-08 11:36:51 -0500

    first commit

---
+++ Class2Prep3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Class2Prep3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] {1,1,2,3,5,8 }; 
+            foreach(int number in numbers)
+            {
+                Console.WriteLine(number);
+                Console.ReadLine();
+            }
+        }
+    }
+}
